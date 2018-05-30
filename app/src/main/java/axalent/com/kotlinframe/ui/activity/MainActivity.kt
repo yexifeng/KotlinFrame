19da7ddc9538c1e853ab33733a446b5b0002258c
simon <simon.xu@axalent.com>
2018-05-30 14:36:23 +0800

    start new dev
    Signed-off-by: simon <simon.xu@axalent.com>

--- app/src/main/java/axalent/com/kotlinframe/ui/activity/MainActivity.kt
+++ app/src/main/java/axalent/com/kotlinframe/ui/activity/MainActivity.kt
@@ -8,16 +8,6 @@ class MainActivity{
         Log.e("LOG_TAG","this is develop branch")
     }
 
-    fun featural_action1(){
-        Log.e("LOG_TAG","simon finish this action")
-    }
 
-    fun dev_featural_action2(){
-        Log.e("LOG_TAG","franky finish acion2")
-    }
-
-    fun dev_featural3(){
-        Log.e("LOG_TAG","meek finish action3")
-    }
 }
 
